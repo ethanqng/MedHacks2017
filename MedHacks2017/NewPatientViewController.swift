fc7ae531235cd61982b9951327f5a517ad2e43ad
Po Tsui <potsui@stanford.edu>
2017-09-09 14:51:47 -0400

    Register new firebase auth user

--- MedHacks2017/NewPatientViewController.swift
+++ MedHacks2017/NewPatientViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Olivia Brown. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 class NewPatientViewController: UIViewController {
@@ -22,5 +23,30 @@ class NewPatientViewController: UIViewController {
         descriptionTwoLabel.adjustsFontSizeToFitWidth = true
         getStartedButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
+    }
+
+    @IBAction func registerNewUser(_ sender: AnyObject) {
+        // Fetch information from form
+        let username = usernameField.text
+        let firstName = firstNameField.text
+        let lastName = lastNameField.text
+        let email = emailField.text
+        let password = passwordField.text
+        let doctorEmail = doctorEmailField.text
+
+        // Validate information
+        if username != "" && firstName != "" && lastName != "" && email != "" && password != "" && doctorEmail != "" {
+            //Create user in firebase
+            Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+                if error == nil {
+                    self.showMessagePrompt("You are now registered!")
+                } else {
+                    self.showMessagePrompt(error.localizedDescription)
+                    return
+                }
+            }
+        } else {
+            self.showMessagePrompt("Please fill out all fields.")
+        }
     }
 }
